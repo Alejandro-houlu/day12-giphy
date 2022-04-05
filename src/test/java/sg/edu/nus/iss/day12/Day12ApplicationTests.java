ae1e44c30fa0ab6a4b87a4d5ba09f5f8295cedfa
Alejandro-houlu <alejandro.hlcw@gmail.com>
2022-04-05 16:30:15 +0800

    gha5

--- src/test/java/sg/edu/nus/iss/day12/Day12ApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/day12/Day12ApplicationTests.java
@@ -1,13 +1,25 @@
 package sg.edu.nus.iss.day12;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import sg.edu.nus.iss.day12.Services.GiphyImplementation;
 
 @SpringBootTest
 class Day12ApplicationTests {
 
+	@Autowired
+	private GiphyImplementation gService;
+
 	@Test
-	void contextLoads() {
+	void shouldLoad10Images() {
+		List<String> gifs = gService.getSearchResult("dog");
+		//assertEquals(10, gifs.size(), "Default number of gifs");
+		assertEquals(10, gifs.size(), "Default number of gifs");
 	}
 
 }
